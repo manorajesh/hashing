3f4448c5a61b67cf369fc48caf3c28066897e6a2
manorajesh <mano@raje.sh>
2022-05-15 16:15:20 -0700

    Initial commit

---
+++ hash.py
@@ -0,0 +1,19 @@
+plaintext = "hello world"
+
+def hashing(plaintext, length=32):
+    seed = 0
+    hash = []
+    salt = 0
+    random_length_num = 1
+    text = "abcdefghjiklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ)(*&%$#@!"
+
+    for char in plaintext:
+        random_length_num += ord(char)
+
+    while salt <= length:
+        seed += ord(plaintext[salt % len(plaintext)]) + salt * random_length_num
+        hash.append(text[(seed**salt*random_length_num) % len(text)])
+        salt += 1
+    return hash
+
+print("".join(hashing(plaintext)))
