64115b9e8bec35804eef28d7a2dcdd87d558ca62
manorajesh <mano@raje.sh>
2022-05-16 11:26:46 -0700

    Fixing test.py for /docs change

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,22 @@
 import random
 import string
-from hash import hashing
+
+def hashing(plaintext, length=32):
+    seed = 0
+    hash = []
+    salt = 0
+    random_length_num = 1
+    text = "abcdefghjiklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ)(*&%$#@!"
+
+    for char in plaintext:
+        random_length_num += ord(char)
+
+    while salt <= length:
+        seed += ord(plaintext[salt % len(plaintext)]) + salt * random_length_num
+        hash.append(text[(seed**salt*random_length_num) % len(text)])
+        salt += 1
+        random_length_num += 1
+    return hash
 
 def lowercase_word():
     word = ''
@@ -12,4 +28,4 @@ def lowercase_word():
 file1 = open("rainbowtabble.txt", "a")
 while True:
     c = lowercase_word()
-    file1.write("%s\t\t>\t%s\n" %(c, "".join(hashing(c))))
+    file1.write("%s\t\t\t>\t\t%s\n" %(c, "".join(hashing(c))))
