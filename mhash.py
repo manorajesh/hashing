708680d9f04296319406a79c184e447082714f7c
manorajesh <mano@raje.sh>
2022-05-18 12:26:11 -0700

    Update mhash.py to new hashing algorithm

--- mhash.py
+++ mhash.py
@@ -13,17 +13,18 @@ def hashing(plaintext, length=32):
     hash = []
     salt = 0
     random_length_num = 1
-    text = "abcdefghjiklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ)(*&%$#@!"
+    text = "abcdefghjiklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ)(*&%$#@!<>?"
 
     for char in plaintext:
         random_length_num += ord(char)
+        random_length_num = ~ random_length_num + (random_length_num << 15) & 0xFFFFFFFF
 
     while salt <= length:
         seed += ord(plaintext[salt % len(plaintext)]) + salt * random_length_num
         hash.append(text[(seed**salt*random_length_num) % len(text)])
         salt += 1
         random_length_num += 1
-    click.echo(base64.b64encode("".join(hash).encode()).decode())
+    return hash
 
 if __name__ == '__main__':
     hashing()
