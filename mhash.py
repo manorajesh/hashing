8106337bcbe601f2b8f197894880273b4ccc9141
manorajesh <mano@raje.sh>
2022-05-18 09:55:05 -0700

    Update mhash.py to new hashing version

--- mhash.py
+++ mhash.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import click
+import base64
 
 @click.command()
 @click.option('--length', '-l', default=32, help='Length of cipertext', show_default=True)
@@ -7,8 +8,7 @@ import click
 @click.version_option(version='0.0.1')
 @click.help_option('--help', '-h')
 
-def hashing(plaintext, length):
-    """Hash PLAINTEXT with a given LENGTH."""
+def hashing(plaintext, length=32):
     seed = 0
     hash = []
     salt = 0
@@ -22,9 +22,8 @@ def hashing(plaintext, length):
         seed += ord(plaintext[salt % len(plaintext)]) + salt * random_length_num
         hash.append(text[(seed**salt*random_length_num) % len(text)])
         salt += 1
-        random_length_num += 3
-    
-    click.echo("".join(hash))
+        random_length_num += 1
+    click.echo(base64.b64encode("".join(hash).encode()).decode())
 
 if __name__ == '__main__':
     hashing()
