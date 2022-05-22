55e13fa4cd24c252e98d050c4be93c197f547885
manorajesh <mano@raje.sh>
2022-05-22 10:33:35 -0700

    Reworked how input is given from user for mhash.py

--- mhash.py
+++ mhash.py
@@ -4,18 +4,17 @@ import base64
 import sys
 
 @click.command()
-@click.argument('plaintext')
-@click.option('-f', '--file', help='File to read from', default="")
+@click.argument('plaintext', type=click.File('r'), default=sys.stdin)
 @click.option('--length', '-l', default=32, help='Length of cipertext', show_default=True)
 @click.option('--encoding', '-e', help='Output encoding', default=None)
 @click.version_option(version='0.0.2')
 @click.help_option('--help', '-h')
 
-def hashing(plaintext, file, length=32, encoding=None):
-    """Hash the PLAINTEXT with a given LENGTH"""
+def hashing(plaintext, length=32, encoding=None):
+    """Hash the FILE, or if no FILE is given, read standard input, and print the hash to standard output."""
 
-    if file != "":
-        plaintext = file.read()
+    if plaintext == sys.stdin:
+        plaintext = plaintext.read()
 
     seed = 0
     hash = []
