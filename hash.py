1e944706a88ba43a2c5cfc95ca27ccf47c190467
manorajesh <mano@raje.sh>
2022-05-24 10:30:38 -0700

    Update hashing algorithm

--- hash.py
+++ hash.py
@@ -1,37 +1,36 @@
 import jellyfish as jf
 
 def hashing(plaintext, length=32):
-    seed = 0
+    cardamom = len(plaintext)
     hash = []
     salt = 0
     pepper = 0
-    text = "abcdefghjiklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ)(*&%$#@!<>?"
+    counter = 0
+    text = "abcdefghjiklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ)(*&%$#@!<?"
 
     for char in plaintext:
         pepper += ord(char) + pepper
-        pepper += ~ len(plaintext)
+        pepper -= ~ len(plaintext)
 
-    while salt <= length:
-        seed += ord(plaintext[salt % len(plaintext)]) + salt * pepper
-        hash.append(text[(seed**salt*pepper) % len(text)])
-        salt += 1
-        pepper += salt
-    return hash
-
-plaintext1 = "f"
-plaintext2 = "ff"
-
-counter = 0
-while True:
-    if hashing(plaintext1) == hashing(plaintext2):
-        print("Collision found")
-        print(plaintext1)
-        print(plaintext2)
-        print(counter)
-        break
-    else:
-        plaintext2 += "f"
+    while counter <= length:
+        cardamom += pepper * cardamom & salt
+        pepper += 2
+        salt -= ~ len(plaintext)
         counter += 1
+        pepper %= cardamom
+    
+    counter = 0
+    plaintext = plaintext.encode()
+    while counter <= length:
+        cardamom = (salt << pepper) | (cardamom >> pepper)
+        hash.append(text[(cardamom*salt*pepper) % len(text)])
+        pepper += plaintext[counter % len(plaintext)]
+        salt += 1
+        counter += 1
+    return hash 
+
+plaintext1 = "xg"
+plaintext2 = "xgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxgxg"
 
 print("".join(hashing(plaintext1)))
 print("".join(hashing(plaintext2)))
---
+++ tests/pyplot_from_file.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+file = open("tests/hash_times.txt", "r")
+times = file.read().split(",")
+print(times)
