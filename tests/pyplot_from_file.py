dd2b8d1d3681e848cb5931b83413b849833633f9
manorajesh <mano@raje.sh>
2022-05-24 11:44:35 -0700

    Update plotting for new features

--- tests/pyplot_from_file.py
+++ tests/pyplot_from_file.py
@@ -1,5 +1,27 @@
 import matplotlib.pyplot as plt
+from scipy.ndimage.filters import gaussian_filter1d
+import numpy as np
 
-file = open("tests/hash_times.txt", "r")
-times = file.read().split(",")
-print(times)
+def line_of_best_fit(x1, x2, y1, y2):
+    b = (y2/y1)**(1/(x2-x1))
+    a = y1/b**x1
+    print(f"y = {a}*{b}^x")
+    
+    values = [a*b**i for i in range(x2)]
+    return values
+
+file = open("tests/hash_times_20000.txt", "r")
+times = file.read().split()
+times = [float(i) for i in times]
+
+times_smoothed = gaussian_filter1d(times, sigma=25)
+lineofbestfit = line_of_best_fit(0, len(times_smoothed), times_smoothed[0], times_smoothed[-1])
+
+plt.plot(times_smoothed)
+plt.plot(lineofbestfit)
+ax = plt.gca()
+
+ax.set_yticks(ax.get_yticks()[::1])
+plt.grid(False)
+plt.ylabel("Time")
+plt.show()
